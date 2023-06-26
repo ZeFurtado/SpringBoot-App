1c1ea795b3bb8982ac43a2030685532df6c83180
ZeFurtado <lukhasfurtado9@gmail.com>
2023-06-26 10:47:59 -0300

    Testando Push do Linux

--- cardapio/src/main/java/com/example/cardapio/controller/BookController.java
+++ cardapio/src/main/java/com/example/cardapio/controller/BookController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("food")
-public class FoodController {
+@RequestMapping("book")
+public class BookController {
     @GetMapping
     public void getAll(){
 
